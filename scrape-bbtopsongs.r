09b015b7210bc37bbbce918e983310ecc09eade4
Vince Ficarrotta <vficarrotta@gmail.com>
2024-07-29 11:04:14 -0500

    Update scrape-bbtopsongs.r

--- scrape-bbtopsongs.r
+++ scrape-bbtopsongs.r
@@ -13,10 +13,11 @@ library('rvest'); library('tidyverse'); library('RSelenium'); library('netstat')
 url1 <- 'https://en.wikipedia.org/wiki/Billboard_year-end_top_30_singles_of_'
 url2 <- 'https://en.wikipedia.org/wiki/Billboard_year-end_top_50_singles_of_'
 url3 <- 'https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_'
+url4 <- 'https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number_ones_of_'
 
 sit <- 1950
 stand <- list()
-for(i in 1:(2022-1950)){
+for(i in 1:(2024-1950)){
     if(sit <= 1955){
         url <- paste0(url1, sit)
         kneel <- read_html(url) %>%
@@ -38,8 +39,8 @@ for(i in 1:(2022-1950)){
         stand[[i]] <- kneel
         sit <- sit + 1
         print(sit)
-    }   
-    else {
+    }
+    else if(sit >= 1959 & sit < 2022){
         url <- paste0(url3, sit)
         kneel <- read_html(url) %>%
             html_element('table.wikitable') %>%
@@ -49,6 +50,22 @@ for(i in 1:(2022-1950)){
         stand[[i]] <- kneel
         sit <- sit + 1
         print(sit)
+    }
+    else{
+    url <- paste0(url4, sit)
+            kneel <- read_html(url) %>%
+                html_element('table.wikitable.plainrowheaders') %>%
+                html_table()
+            colnames(kneel) <- c('Number', 'Issue Date', 'Song', 'Artists', 'Ref')
+            kneel <- kneel[,c(3,4)]
+            kneel <- unique(kneel)
+            kneel["Number"] <- rep(1:length(kneel[,1]), length(kneel[,1]))
+            kneel['Year'] <- rep(sit, length(kneel[,1]))
+            kneel <- tibble(kneel$Number, kneel$Song, kneel$Artists, kneel$Year)
+            colnames(kneel) <- c('Number','Song','Artists','Year')
+            stand[[i]] <- kneel
+            sit <- sit + 1
+            print(sit)
     }
 }
 
@@ -72,6 +89,6 @@ for(i in stand){
 bow$Song <- gsub("[^A-Za-z0-9 '!?$&*@#()-_=.]", "", bow$Song)
 
 ### Data storage checkpoint 1
-# write.csv(stand, '~/1950-2023_bbtopsongs_clean.csv')
-# saveRDS(bow, '~/1950-2023_bbtopsongs_clean.rds')
-stand <- readRDS('~/1950-2023_bbtopsongs_clean.rds')
+# write.csv(stand, '~/1950-2023_bbtopsongs_clean_2023.csv')
+# saveRDS(bow, '~/1950-2023_bbtopsongs_clean_2023.rds')
+stand <- readRDS('~/1950-2023_bbtopsongs_clean2023.rds')
